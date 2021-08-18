fca3a64638111d884b04ec165e32554a5b0a3e75
Vincent He <vincent0710@Outlook.com>
2021-08-18 15:25:49 -0400

    Fixed wrong vertex calcuation

--- res/shader/vertexShader.glsl
+++ res/shader/vertexShader.glsl
@@ -6,5 +6,5 @@ uniform mat4 model;
 uniform mat4 view;
 
 void main(){
-    gl_Position = projection * model * view * vec4(aPos, 1.0f);
+    gl_Position = projection * view * model * vec4(aPos, 1.0f);
 }
