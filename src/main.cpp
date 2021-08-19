297ce82292497702908a89c9c8aa8ad14110dcbc
Vincent He <vincent0710@Outlook.com>
2021-08-19 12:37:13 -0400

    Fixed typs - camear_g to camera_g

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 
 constexpr int width_g {800}; 
 constexpr int height_g {500}; 
-Camera camear_g {glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f)};
+Camera camera_g {glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f)};
 glm::mat4 projection_g {glm::perspective(glm::radians(45.0f), static_cast <float> (width_g) / height_g, 0.1f, 200.0f)};
 
 void init()
